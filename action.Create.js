83ddcdeb72e75cabff04d87254c5c20cead56841
Jay Goodfellow <j.goodfellow@arcane.ws>
2016-11-22 09:02:12 -0500

    moved the creating of creeps into its own module

--- action.Create.js
+++ action.Create.js
@@ -1,8 +1,18 @@
 module.exports = {
-    run:  function(creep) {
-        let source = creep.pos.findClosestByRange(FIND_SOURCES)
-        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
+    run:  function(workForce) {
+      let spawn1 = Game.spawns['HQ']
+      if(spawn1.canCreateCreep){
+        let spawnName = 'Spawn_' + Game.time
+
+  			if (workForce.Harvester < 1) {
+  				spawn1.createCreep([WORK,MOVE,MOVE,CARRY,CARRY], spawnName, {'role':'Harvester'})
+        } else if(workForce.Upgrader < 3) {
+          spawn1.createCreep([WORK,WORK,MOVE,MOVE,CARRY,CARRY], spawnName, {'role':'Upgrader'})
+        } else if (workForce.Builder < 2 && !_.isEmpty(Game.constructionSites)) {
+          spawn1.createCreep([WORK,WORK,MOVE,MOVE,CARRY,CARRY], spawnName, {'role':'Builder'})
+        } else if (workForce.Fixer < 2) {
+          spawn1.createCreep([WORK,MOVE,MOVE,CARRY,CARRY,CARRY], spawnName, {'role':'Fixer'})
         }
+      }
     }
 };
